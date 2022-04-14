71aabe1a53fd74ce22785804713958e4ca51a711
Ben Taft <ben.taft@career2cradle.org>
2022-04-14 16:57:30 -0500

    parsing laus data is a little more sophisticated now.

--- src/herc/bls/laus/constants.py
+++ src/herc/bls/laus/constants.py
@@ -1,6 +1,8 @@
 #  Copyright (c) 2022 by Higher Expectations for Racine County.
 r"""Internal constants for dealing with Local Area Unemployment Statistics"""
 
+from pandas.api.types import CategoricalDtype
+
 URL = 'https://api.bls.gov/publicAPI/v2/timeseries/data/'
 
 JSON_CONTENT_TYPE = {
@@ -14,4 +16,26 @@ OUTPUT_COLUMNS = {
     'value': 'Value'
 }
 
+MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+]
+
+OUTPUT_DTYPES = {
+    'Series': 'str',
+    'Year': 'int32',
+    'Month': CategoricalDtype(categories=MONTHS,
+                              ordered=True),
+    'Value': 'float32'
+}
 
--- src/herc/bls/laus/wrangle_data.py
+++ src/herc/bls/laus/wrangle_data.py
@@ -8,16 +8,19 @@ from typing import (
 import pandas
 import requests
 
-from .constants import OUTPUT_COLUMNS
+from .constants import (
+    OUTPUT_COLUMNS,
+    OUTPUT_DTYPES,
+)
 
 
-def _consolidate(d):
-    df = pandas.DataFrame(d['data'])
-    df.insert(0, 'series', d['seriesID'])
-    return df
+def _wrangle_series(extract):
+    df = pandas.DataFrame(extract['data'])
+    df.insert(0, 'series', extract['seriesID'])
+    return df[OUTPUT_COLUMNS.keys()]
 
 
-def _extract_data_payload(response: requests.Response) -> Iterable:
+def _extract_series(response: requests.Response) -> Iterable:
     return filter(lambda x: bool(x['data']),
                   response.json()['Results']['series'])
 
@@ -37,10 +40,10 @@ def wrangle(responses: List[requests.Response]) -> pandas.DataFrame:
 
     """
 
-    tables = [pandas.concat(map(_consolidate,
-                                _extract_data_payload(r)))
-              for r in responses]
+    tables = [pandas.concat(map(_wrangle_series,
+                                _extract_series(response)))
+              for response in responses]
 
-    return pandas.concat(tables)[OUTPUT_COLUMNS.keys()].\
-        convert_dtypes().\
-        rename(columns=OUTPUT_COLUMNS)
+    return pandas.concat(tables).\
+        rename(columns=OUTPUT_COLUMNS).\
+        astype(OUTPUT_DTYPES)
